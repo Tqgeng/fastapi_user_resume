6dc4af5dfd2102ab4e203463cdfbef9914cb2346
Tqgeng <pavel.kramarew@mail.ru>
2025-09-02 13:32:50 +0300

    add core models, schemas, authentication

---
+++ fastapi-application/core/models/user.py
@@ -0,0 +1,18 @@
+from typing import TYPE_CHECKING
+
+from fastapi_users.db import SQLAlchemyBaseUserTable, SQLAlchemyUserDatabase
+
+
+from core.types.user_id import UserIdType
+from .base import Base
+from .mixin.int_id_pk import IdIntPkMix
+
+if TYPE_CHECKING:
+    from sqlalchemy.ext.asyncio import AsyncSession
+
+
+class User(Base, IdIntPkMix, SQLAlchemyBaseUserTable[UserIdType]):
+
+    @classmethod
+    def get_db(cls, session: "AsyncSession"):
+        return SQLAlchemyUserDatabase(session, User)
