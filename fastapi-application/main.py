0cb857cc6e8c9b82f2ceda7da9b70aff6b6d4fdf
Tqgeng <pavel.kramarew@mail.ru>
2025-09-03 14:47:15 +0300

    add pages router

--- fastapi-application/main.py
+++ fastapi-application/main.py
@@ -1,7 +1,11 @@
 from contextlib import asynccontextmanager
+from pathlib import Path
+
 import uvicorn
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
 
+from api.api_v1 import pages
 from core.config import settings
 from core.models import db_helper
 
@@ -16,8 +20,15 @@ async def lifespan(app: FastAPI):
     await db_helper.dispose()
 
 
+BASE_DIR = Path(__file__).resolve().parent
+STATIC_DIR = BASE_DIR / "static"
+
 main_app = FastAPI(lifespan=lifespan)
 main_app.include_router(api_router)
+main_app.include_router(pages.router)
+
+main_app.mount("/static", StaticFiles(directory=str(STATIC_DIR)), name="static")
+
 
 if __name__ == "__main__":
     uvicorn.run(
